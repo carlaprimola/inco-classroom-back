6182c7ee16ec83751b403eeea76208d2ecd22bef
soyshan <shanbu131@gmail.com>
2024-03-14 18:00:01 +0100

    backend_TeacherPermissions002_Adding restrictions in the routes of courses and changes in the coursescontroller

--- routes/courses.js
+++ routes/courses.js
@@ -3,13 +3,15 @@ import { createCourse, deleteCourse, updateCourse, getCourseById, getCourses } f
 import { checkPermissions } from "../middleware/checkPermissions.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
-const router = express.Router();
 
 // Rutas protegidas para el controlador de cursos
-router.get('/', getCourses);
-router.get('/:id', getCourseById);
+const router = express.Router();
+router.get('/', verifyToken, getCourses);
+router.get('/:id', verifyToken, getCourseById);
 router.post('/', verifyToken, checkPermissions('Docente'), createCourse);
 router.put('/:id', verifyToken, checkPermissions('Docente'), updateCourse);
 router.delete("/:id", verifyToken, checkPermissions('Docente'), deleteCourse);
 
+
 export default router;
+
