b470c89c29adeea756a34ec0ba3112bedb1e13a0
soyshan <shanbu131@gmail.com>
2024-03-17 21:56:14 +0100

    backend_ConnectFront001_Changes in authcontroller and users routes

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -21,7 +21,8 @@ export const loginUser = async (req, res) => {
         const token = jwt.sign({ userId: user.ID, email: user.Email }, 'secret_key', { expiresIn: '3h' });
 
         // Enviamos la respuesta con el token y los datos del usuario
-        res.status(200).json({ message: "Inicio de sesión correcto", token });
+        res.status(200).json({ message: "Inicio de sesión correcto", token, user: { id: user.ID, email: user.Email, name: user.Nombre, role: user.Role } });
+
     } catch (error) {
         res.status(500).json({ message: "Error en el inicio de sesión", error });
     }
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,29 @@
+// import express from "express";
+// import { createUser, deleteUser, updateUser, getUserById, getUsers } from "../controllers/UsersController.js";
+
+
+
+// const routes = express.Router();
+
+// routes.get('/', getUsers);
+// routes.get('/:id', getUserById);
+// routes.post('/', createUser);
+// routes.put('/:id', updateUser);
+// routes.delete("/:id", deleteUser);
+
+// export default routes;
+
+
 import express from "express";
+import { verifyToken } from "../middleware/verifyToken.js"; 
 import { createUser, deleteUser, updateUser, getUserById, getUsers } from "../controllers/UsersController.js";
-
-
 
 const routes = express.Router();
 
-routes.get('/', getUsers);
+// Ruta protegida con el middleware verifyToken
+routes.get('/', verifyToken, getUsers);
+
+// Otras rutas sin protección
 routes.get('/:id', getUserById);
 routes.post('/', createUser);
 routes.put('/:id', updateUser);
