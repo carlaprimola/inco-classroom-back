b86de24748da14aa314c9c4dba4099a65b11689a
Carla Escobar <carlaprimola@outlook.es>
2024-03-24 19:10:43 +0100

    backend_student001_new url to filter course by student

--- routes/student.js
+++ routes/student.js
@@ -1,13 +1,14 @@
 // student.js
 import express from 'express';
 // import authenticateStudent from '../middleware/authenticateStudent.js';
-import { getStudentById, getStudents } from '../controllers/StudentController.js';
+import { getStudentById, getStudents, getStudentCourses } from '../controllers/StudentController.js';
 import {verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/', getStudents);
 router.get('/:id',  getStudentById);
+router.get('/:id/courses', getStudentCourses);
 
 // authenticateStudent,
 export default router;
