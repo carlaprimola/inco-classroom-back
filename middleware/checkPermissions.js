ab6011849961223ca305197ebe2c43fa4cd34207
soyshan <shanbu131@gmail.com>
2024-03-26 21:55:20 +0100

    backend_DeleteFiles_Delete a middleware that its not working

--- middleware/checkPermissions.js
+++
@@ -1,9 +0,0 @@
-export const checkPermissions = (requiredRole) => {
-    return (req, res, next) => {
-        const { role } = req.user;
-        if (role !== requiredRole) {
-            return res.status(403).json({ message: "Acceso no autorizado" });
-        }
-        next();
-    };
-};
--- routes/courses.js
+++ routes/courses.js
@@ -1,15 +1,6 @@
 import express from "express";
 import { createCourse, deleteCourse, updateCourse, getCourseById, getCourses } from "../controllers/CoursesController.js";
-import { checkPermissions } from "../middleware/checkPermissions.js";
 import { verifyToken } from "../middleware/verifyToken.js";
-
-// const router = express.Router();
-// router.get('/',getCourses);
-// router.get('/:id', getCourseById);
-// router.post('/', createCourse);
-// router.put('/:id',  updateCourse);
-// router.delete("/:id",  deleteCourse);
-
 
 
 // Rutas protegidas para el controlador de cursos
@@ -17,10 +8,10 @@ import { verifyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 router.get('/',verifyToken, getCourses);
 router.get('/:id',verifyToken, getCourseById);
-//asegurando que el usuario ha iniciado sesión y que solo puede acceder el docente
-router.post('/', verifyToken, checkPermissions('Docente'), createCourse);
-router.put('/:id', verifyToken, checkPermissions('Docente'), updateCourse);
-router.delete("/:id", verifyToken, checkPermissions('Docente'), deleteCourse);
+
+router.post('/', verifyToken,  createCourse);
+router.put('/:id', verifyToken,  updateCourse);
+router.delete("/:id", verifyToken,  deleteCourse);
 
 
 export default router;
